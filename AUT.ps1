addafe05d2b70a0a9bc040d5ffa89f502702e7d6
Alex Owings <owings.alex@gmail.com>
2019-08-29 18:49:03 -0600

    Update AUT.ps1

--- AUT.ps1
+++ AUT.ps1
@@ -1,11 +1,58 @@
-Out-String("Hello! This tool is designed to help you completely 
+Write-host "Hello! This tool is designed to help you completely 
 remove Microsoft Office products 2010 and higher.
-Please save any documents you may have open before continuing.")
+Please save any documents you may have open before continuing."-ForegroundColor Blue
 Write-host "Would you like to to continue (Default is No)" -ForegroundColor Yellow 
     $Readhost = Read-Host " ( y / n ) " 
     Switch ($ReadHost) 
      { 
-       Y {Write-host "Yes, Download PublishSettings"; $yes=$true} 
-       N {Write-Host "No, Skip PublishSettings"; $yes=$false} 
-       Default {Write-Host "Default, Skip PublishSettings"; $yes=$false} 
-     } 
+       Y {Write-host "Yes"; $yes1=$true} 
+       N {Write-Host "No"; $yes1=$false} 
+       Default {Write-Host "Default, Skip PublishSettings"; $yes1=$false} 
+     }
+     if($yes1=$true){
+      Write-host "Do you use Outlook?"-ForegroundColor Blue
+     $Readhost = Read-Host " ( y / n ) " 
+    Switch ($ReadHost) 
+     { 
+       Y {Write-host "Yes"; $yes2=$true} 
+       N {Write-Host "No"; $yes2=$false} 
+       Default {Write-Host "Default, Skip PublishSettings"; $yes2=$false} 
+     }}  elseif($yes1=$false){
+      Write-host "Quitting" -ForegroundColor Pink
+      exit
+     }
+     elseif($yes2=$true){
+      Write-host"Does your profile currently work?"-ForegroundColor Blue
+     $Readhost = Read-Host " ( y / n ) " 
+    Switch ($ReadHost) 
+     { 
+       Y {Write-host "Yes"; $yes3=$true} 
+       N {Write-Host "No"; $yes3=$false} 
+       Default {Write-Host "Default, Skip PublishSettings"; $yes3=$false} 
+     } } elseif($yes2=$false){
+      Write-host"do you want to start the process?"-ForegroundColor Blue
+     $Readhost = Read-Host " ( y / n ) " 
+    Switch ($ReadHost) 
+     { 
+       Y {Write-host "Yes"; $yes4=$true} 
+       N {Write-Host "No"; $yes4=$false} 
+       Default {Write-Host "Default, Skip PublishSettings"; $yes4=$false} 
+     } }elseif($yes3=$true){
+      $logFile = 'outlookprofiles.csv'
+
+If (Test-Path 'hkcu:\Software\Microsoft\Office\15.0\Outlook\Profiles') {
+	$regPath = 'hkcu:\Software\Microsoft\Office\15.0\Outlook\Profiles\*\9375CFF0413111d3B88A00104B2A6676\*'
+}elseif(Test-Path 'hkcu:\Software\Microsoft\Office\16.0\Outlook\Profiles'){
+	$regPath = 'hkcu:\Software\Microsoft\Office\16.0\Outlook\Profiles\*\9375CFF0413111d3B88A00104B2A6676\*'
+}else{
+	Write-Host 'Outlook not found'
+	exit
+}
+
+Get-ItemProperty  $regPath |
+	Where-Object{$_.'Account Name' -notmatch 'Outlook Address Book|Outlook Data File'} |
+	Select-Object @{n= 'ComputerName';e={$env:COMPUTERNAME}},@{n= 'UserName';e={$env:USERNAME}}, 'Account Name' |
+	Export-Csv $logFile 
+     }
+     ForEach-Object{Add-Content -Path .\outlookprofiles.csv}
+     Get-Content -Path .\outlookprofiles.csv
